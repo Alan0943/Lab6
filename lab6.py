66c92ef3b0c5e82cf6a426ec7941835555f8285e
Alan <askrypek@ufl.edu>
2023-10-25 21:11:43 -0400

    changes

--- lab6.py
+++ lab6.py
@@ -7,7 +7,22 @@ def encoder(x):
         else:
             new_pass = new_pass + str((int(i) + 3))
     return x
-pass_input = encoder(input("What is your password? "))
 
-print(pass_input)
-print("new changes")
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    option_input = input("Please enter an option: ")
+
+    if option_input == 3:
+        quit()
+
+    elif option_input == 1:
+        pass_input = encoder(input("Please enter your password to encode: "))
+        print("Your password has been encoded and stored!")
+
+    elif option_input == 2:
+        pass
