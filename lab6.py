74e23b4ddf837767173fc36916e59d13c57b8220
Alan <askrypek@ufl.edu>
2023-10-25 21:03:27 -0400

    new code

--- lab6.py
+++ lab6.py
@@ -1,12 +1,13 @@
-pass_input = input("What is your password? ")
+#Alan Skrypek
+def encoder(x):
+    new_pass = ""
+    for i in x:
+        if int(i) + 3 > 10:
+            new_pass += str((int(i) + 3) % 10)
+        else:
+            new_pass = new_pass + str((int(i) + 3))
+    return x
+pass_input = encoder(input("What is your password? "))
 
-new_pass = ""
-
-for i in pass_input:
-    if int(i) + 3 > 10:
-        new_pass += str((int(i) + 3) % 10)
-    else:
-        new_pass = new_pass + str((int(i) + 3))
-
-print(new_pass)
+print(pass_input)
 print("new changes")
